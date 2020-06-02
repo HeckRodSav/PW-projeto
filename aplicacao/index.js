0141bfcbf6ea64f91c88859d20856c890d68e267
HeckRodSav <heitor.rodrigues.savegnago@outlook.com>
2020-06-02 20:05:28 -0300

    safe load dev packages

--- aplicacao/index.js
+++ aplicacao/index.js
@@ -3,27 +3,63 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const livereload = require('livereload');
-const connectLivereload = require('connect-livereload');
+const process = require('process');
+
+const livereload = (() => {
+    var ret = null;
+    try { ret = require('livereload');}
+    catch (e) {};
+    return ret;
+    })();
+
+const connectLivereload = (() => {
+    var ret = null;
+    try { if (livereload) ret = require('connect-livereload');}
+    catch (e) {};
+    return ret;
+    })();
 
 // inicializar app express
 const app = express();
-const liveReloadServer = livereload.createServer();
 
+if(livereload !== null){
+    const liveReloadServer = livereload.createServer();
+    const watcher = liveReloadServer.watch(__dirname);
+    watcher.on("change", (arg) => {
+        console.log(`Change: ${new Date().toLocaleString()} - ${arg.replace(__dirname, "")}`);
+    });
+}
 
-
-
-var watcher = liveReloadServer.watch(__dirname);
-
-watcher.on("change", (arg) => {
-    console.log(`Change: ${new Date().toLocaleString()} - ${arg.replace(__dirname, "")}`);
-});
-
-
-
-let port = 4519;
+const port = 4519;
 app.listen(port, () => {
-    console.log('Servidor em execução port' + port);
+    console.log(`ervidor em execução port ${port}`);
 });
 
-app.use(connectLivereload()); //
+if(connectLivereload !== null){
+    app.use(connectLivereload());
+}
+
+
+
+
+
+
+// error handling middleware
+app.use((err, req, res, next) => {
+	console.log(err);
+	console.error(err.stack);
+	// res.status(500).sendFile(ERROR_PATH);
+});
+
+// default route after everything fails
+app.use('/', (req, res) => {
+	// res.status(404).sendFile(NOT_FOUND_PATH);
+});
+
+// register exitting log
+process.on('exit', (code) => {
+	console.log(`\nServer exiting with code ${code}`);
+});
+
+process.on('SIGINT', process.exit);
+process.on('SIGUSR2', process.exit); //Salvar com nodemon
