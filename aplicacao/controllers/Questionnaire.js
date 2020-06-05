9394dd5117890940b7870c7998a866546ac165dd
otaviols <otavio.l.s@hotmail.com>
2020-06-05 00:16:28 -0300

    questionnaire controler has been created, to manage the requests of questions and related.

---
+++ aplicacao/controllers/Questionnaire.js
@@ -0,0 +1,25 @@
+"use strict";
+
+const model = require('../models/symptom-model.js');
+
+exports.GetQuestion = (req, res) => {
+//retorna a view montando a questão com o objeto de sintoma que esse controler recebe no res
+
+};
+
+
+
+exports.Answer = (req, res) =>{
+//tentar criar o m´metodo find by id e find by tag nas models
+const userSymptom=model.findbyid(res.body.idSymptom);
+
+if(res.body.Answer.val=="Sim"){
+//fazer a lógica de buscar doenças com o mesmo sintoma no banco e montar uma lista    
+}
+    else{
+//ver a lista de sintomas que estão como positivo na sessão e buscar doenças que ainda não foram perguntadas
+
+    }
+
+    exports.GetQuestion('', /*objeto encontrado na lógica*/);
+};
